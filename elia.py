798af35d8b763b87cec57157a73407beda4a65e6
Elia Partin <arenaselia@gmail.com>
2023-06-16 12:11:48 -0400

    created file structure

---
+++ elia.py
@@ -0,0 +1,10 @@
+
+
+class Elia:
+       def __init__(self):
+          pass
+       def past(self):
+              print("You have chosen the PAST and find yourself in the Dark Ages."
+              "You see a horse in the alley way where you have landed in your time machine."
+              "Do you want to take the HORSE or WALK to start your journey through the PAST?")
+
--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-class Prompt {
-    def __init__(self, opt1, opt2):
-        self.opt1 = opt1
-        self.opt2 = opt2
-}
+from elia import Elia
+import vaughn
+import jay
 
-star_game = Prompt("")
+text = ("Welcome to the Time Adventure Game!" 
+     "You are a lonely scientist," 
+     "who has stumbled upon the solution to time travel."
+     "You must now decide where your first adventure shall take you!"
+     "Type START, to start the game or HELP for help."
+     "Type QUIT to end the game at anytime.")
+print(text)
+player_input = ""
+
+def play_elia():
+        elia = Elia()
+        return elia.past()
+
+# class Prompt:
+#     def __init__(self, opt1, opt2, opt3):
+#         self.opt1 = opt1
+#         self.opt2 = opt2
+#         self.opt3 = opt3
+    
+   
+
+def play(player_input):
+    while player_input.upper() != "QUIT":
+        # print() 
+        player_input = input("You enter your time machine and it asks you for a destination: PAST, PRESENT or FUTURE. ")
+        if player_input.upper() == "PAST":
+            print("PAST")
+            return play_elia()
+        elif player_input.upper() == "PRESENT":
+            print("PRESENT")
+            #run vaughn.py
+        elif player_input.upper() == "FUTURE":
+            print("FUTURE")
+            #run jay.py
+       
+            
+
+# start_game = Prompt("PAST", "PRESENT", "FUTURE")
+play(player_input)
