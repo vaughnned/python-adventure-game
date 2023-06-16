d9ec1f194cc3568831507527fe711242ac731866
Elia Partin <arenaselia@gmail.com>
2023-06-16 14:42:49 -0400

    added function

--- elia.py
+++ elia.py
@@ -4,7 +4,9 @@ class Elia:
        def __init__(self):
           pass
        def past(self):
-              print("You have chosen the PAST and find yourself in the Dark Ages."
-              "You see a horse in the alley way where you have landed in your time machine."
-              "Do you want to take the HORSE or WALK to start your journey through the PAST?")
-
+              quit = False
+              while not quit:
+                print("You have chosen the PAST and find yourself in the Dark Ages.")
+                player_input = input ("You see a horse in the alley way where you have landed in your time machine."
+                "Do you want to take the HORSE or WALK to start your journey through the PAST?")
+                if player_input == ():
