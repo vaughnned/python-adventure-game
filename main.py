1e074ce7dd82b2e5b5f164bc2bf759740f888098
Elia Partin <arenaselia@gmail.com>
2023-06-19 19:57:01 -0400

    added help directory

--- main.py
+++ main.py
@@ -1,48 +1,65 @@
 from elia import Elia
 from vaughn import Vaughn
-import jay
+from jay import Jay
+from help import Directory
 
-text = ("Welcome to the Time Adventure Game!" 
-     "You are a lonely scientist," 
-     "who has stumbled upon the solution to time travel."
-     "You must now decide where your first adventure shall take you!"
-     "Type START, to start the game or HELP for help."
-     "Type QUIT to end the game at anytime.")
+
+text = (
+    "Welcome to the Time Adventure Game!"
+    "You are a lonely scientist,"
+    "who has stumbled upon the solution to time travel."
+    "You must now decide where your first adventure shall take you!"
+    "Type START, to start the game or HELP for help."
+    "Type QUIT to end the game at anytime."
+)
 print(text)
 player_input = ""
 
+
 def play_elia():
-        elia = Elia()
-        return elia.past()
+    elia = Elia()
+    return elia.past()
+
 
 def play_vaughn():
-        vaughn = Vaughn()
-        return vaughn.present()
+    vaughn = Vaughn()
+    return vaughn.present()
+
+
+def play_jay():
+    jay = Jay()
+    return jay.future()
+
+
+def play_help():
+    help_dir = Directory()
+    help_dir.display_help()
+
 
 # class Prompt:
 #     def __init__(self, opt1, opt2, opt3):
 #         self.opt1 = opt1
 #         self.opt2 = opt2
 #         self.opt3 = opt3
-    
-   
+
 
 def play(player_input):
     while player_input.upper() != "QUIT":
-        # print() 
-        player_input = input("You enter your time machine and it asks you for a destination: PAST, PRESENT or FUTURE. > ")
+        # print()
+        player_input = input(
+            "You enter your time machine and it asks you for a destination: PAST, PRESENT or FUTURE. > "
+        )
         if player_input.upper() == "PAST":
             print("PAST")
             return play_elia()
         elif player_input.upper() == "PRESENT":
             print("PRESENT")
             return play_vaughn()
-            #run vaughn.py
+
         elif player_input.upper() == "FUTURE":
             print("FUTURE")
-            #run jay.py
-       
-            
+        elif player_input.upper() == "HELP":
+            print("HELP")
 
-# start_game = Prompt("PAST", "PRESENT", "FUTURE")
+
 play(player_input)
