4a81930718645b7338d0d73ea1b4641e8ee9fc02
Elia Partin <arenaselia@gmail.com>
2023-06-20 12:46:51 -0400

    fixed help function

--- help.py
+++ help.py
@@ -2,14 +2,14 @@ class Directory:
     def __init__(self):
         pass
 
-        def display_help(self):
-            help_text = ""
-            print( 
-                "Available commands:"
-                "HELP: Display this help message"
-                "QUIT: Exits the game."
-                "PAST: Choose the past as your destination."
-                "PRESENT: Choose the present as your destination"
-                "FUTURE: Choose the future as your destination"
-                "The commands may be typed in lowercase or uppercase."
-            )
+    def display_help(self):
+        help_text = ""
+        print( 
+            "Available commands:"
+            "HELP: Display this help message"
+            "QUIT: Exits the game."
+            "PAST: Choose the past as your destination."
+            "PRESENT: Choose the present as your destination"
+            "FUTURE: Choose the future as your destination"
+            "The commands may be typed in lowercase or uppercase."
+        )
